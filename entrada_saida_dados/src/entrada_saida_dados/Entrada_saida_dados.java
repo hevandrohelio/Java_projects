ec08bf8c7cdcc5660dbdcd0a9912d733c11e1c1f
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2023-08-28 15:47:52 -0300

    Comentários

--- entrada_saida_dados/src/entrada_saida_dados/Entrada_saida_dados.java
+++ entrada_saida_dados/src/entrada_saida_dados/Entrada_saida_dados.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 public class Entrada_saida_dados {
 
     public static void main(String[] args) {
-        System.out.println("A");
-        int num1;
-        int num2;
-        int soma;
-        Scanner input = new Scanner(System.in);
+        int num1; // numero 1
+        int num2; // numero 2
+        int soma; // variável que recebe a soma
+        Scanner input = new Scanner(System.in); //instância para receber dados
         System.out.println("Digite o primeiro valor: ");
         num1 = input.nextInt();
         System.out.println("Digite o segundo valor: ");
         num2 = input.nextInt();
-        soma = num1 + num2;
+        soma = num1 + num2; // soma os números
         System.out.println("A soma de " + num1 + " e " + num2 + " é " + soma);
         
     }
