1de460e72f6f3d0264e0505fb7231e1dfcdc8d98
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2023-08-28 14:53:11 -0300

    Olá, Mundo!
    O clássico projeto "Olá, Mundo!"

---
+++ Ola_mundo/src/ola_mundo/Ola_mundo.java
@@ -0,0 +1,10 @@
+package ola_mundo;
+
+public class Ola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println("O CLÁSSICO! XD");
+    }
+
+}
