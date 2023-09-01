4deabd6b7f47c313ecc1078c1a14d60cd159e505
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2023-09-01 16:48:59 -0300

    programa que calcula a área de um círculo

---
+++ projetos/area_circulo/src/area_circulo/Area_circulo.java
@@ -0,0 +1,22 @@
+// programa que calcula a área do círculo
+package area_circulo;
+
+import java.util.Scanner;
+
+public class Area_circulo {
+
+    public static void main(String[] args) {
+        double r; // raio
+        double A; // área
+         
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Qual o raio do círculo?");
+        r = input.nextFloat();
+        
+        A = ( 3.14 * Math.pow(r,2)); // calcula a área
+        
+        System.out.println("A área do círculo é "+ A);
+    }
+    
+}
